b02cb821804eb031648021581aa383bd5fcc6bf9
Nithin Kumar Putti <puttinithinkumar2000@gmail.com>
2025-04-22 19:34:48 -0500

    Add files via upload

---
+++ chat_system_supabase/supabase/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Space_Grotesk } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+
+const space_Grotesk = Space_Grotesk({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "hedlyner"
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className={space_Grotesk.className}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="dark"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+					<Toaster position="top-center" />
+				</ThemeProvider>
+			</body>
+		</html>
+	);
+}
---
+++ chat_system_supabase/supabase/components/ChatAbout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ChatAbout() {
+	return (
+		<div className="flex-1 flex items-center justify-center">
+			<div className="text-center space-y-5">
+				<h1 className="text-3xl font-bold">Welcome to Hedlyner Chat</h1>
+
+			</div>
+		</div>
+	);
+}
